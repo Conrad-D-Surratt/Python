bf68d7c1efed3c29e2a8533aef6faa090fb41f53
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-06-01 10:45:16 -0500

    Knock Knock Joke

---
+++ knock knock.py
@@ -0,0 +1,8 @@
+"""_summary_
+reference: my parents told me the joke when i was a kid
+"""
+print("Knock, Knock")
+print("Who's There?")
+print("Boo. \nBoo who? \nQuit your crying!")
+#For lines 1-3, I used the triple quotations with the summary, as we were taught earlier in the lesson.
+#For line 6, I decided to try out the \n escape command. Worked out perfectly!
