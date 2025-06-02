ddbad10fef835ca8206db4949e166a5a1ee76852
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-06-01 23:51:10 -0500

    PE1 2.3: Mathematical Operations

---
+++ PE1 2.3 Mathematical Operations.py
@@ -0,0 +1,7 @@
+length = 15
+width = 10
+area = length * width
+print ("The area of a the rectangle with a length of" , length , "and a width of" , width , "is" , area)
+#I could've simply used print to state at least the length and width, but it was more fun this way.
+#I couldn't add a period at the end unfortunately, at least not without using a string concentration, but we haven't learned about those in the modules yet.
+#I know about the string concentrations already because I already finished the entire 2nd module on the Python Institute before starting this assignment.
