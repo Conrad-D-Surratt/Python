db75341558638f672cc42679c293fbe29e1f7c9b
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-06-01 23:35:13 -0500

    PE1 2.2 Assignment

---
+++ Understanding Data Types in Python.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 25)
+
+# Integer
+y = 300000000
+print("Integer:", y)
+
+# Float
+z = 0.00000000001
+print("Float:", z)
+
+# String
+name = "Batman"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
