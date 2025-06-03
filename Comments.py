2fc96857c998e44587bcbbae6726ca9b1315e9e9
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-06-02 23:02:20 -0500

    Comments assignment for module PE1 2.5

---
+++ Comments.py
@@ -0,0 +1,9 @@
+"""Calculate and display miles per gallon using the fuel tank size and distance"""
+# TODO: Make the tank_size and distance user inputs in order to make it into a true calculator
+tank_size = 15 #size of the tank in gallons
+# Define the total distance covered
+distance = 600 #distance in miles
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
